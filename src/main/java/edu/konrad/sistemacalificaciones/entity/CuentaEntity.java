ac8db491cf031d99c90dc1d4c4e95c22866c2283
cafc1234 <camilofique@hotmail.com>
2018-09-09 08:14:43 -0500

    Initial commit

---
+++ src/main/java/edu/konrad/sistemacalificaciones/entity/CuentaEntity.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.konrad.sistemacalificaciones.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Clase encargada de crear la entidad Cuenta
+ * @author camil
+ */
+@Entity(name = "Cuenta")
+public class CuentaEntity {
+    
+}
