988a86c17f43b871550c6d8943de6525c8817c66
cafc1234 <camilofique@hotmail.com>
2018-09-14 18:00:51 -0500

    Nuevas clases persistencia
    Se crea las clases persistencia de:
    Programa
    Rol

---
+++ src/main/java/edu/konrad/sistemacalificaciones/persistence/ProgramaPersistence.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.konrad.sistemacalificaciones.persistence;
+
+import edu.konrad.sistemacalificaciones.entity.ProgramaEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * Clase manejadora de la transaccionalidad de la entidad Programa
+ * @author Evelyn Gúzman y Camilo Fique
+ */ 
+@Stateless
+public class ProgramaPersistence {
+ 
+     /**
+     * Instancia del entity manager
+     */
+    @PersistenceContext(unitName = "calificacionesPU")
+    private EntityManager entityManager;
+
+    /**
+     * Metodo que trae todos los datos que se encuentran en la tabla Programa
+     * @return lista programa
+     */
+
+    public List<ProgramaEntity> findAll(){
+        Query queryProducto = entityManager.createQuery("select p from Programa p");
+        return queryProducto.getResultList();
+    }
+    
+    /**
+     * Metodo que busca un objeto programa  mediante su id
+     * @param id
+     * @return programa encontrado
+     */
+    public ProgramaEntity find(Long id){
+        return entityManager.find(ProgramaEntity.class, id);
+    }
+
+       
+    /**
+     * Metodo para crear un objeto de la entidad programa
+     * @param programaNuevo
+     * @return programaNuevo
+     */
+    public ProgramaEntity create(ProgramaEntity programaNuevo){
+        entityManager.persist(programaNuevo);
+        return programaNuevo;
+    }
+    
+    /**
+     * Metodo para actualizar un dato de la entidad programa
+     * @param programaActualizar
+     * @return programa actualizado
+     */
+    public ProgramaEntity update(ProgramaEntity programaActualizar){
+        return entityManager.merge(programaActualizar);
+    }
+    
+    /**
+     * Metodo usado para eliminar un programa
+     * @param id 
+     */
+    public void remove(Long id){
+        ProgramaEntity programaEliminar = entityManager.find(ProgramaEntity.class, id);
+        entityManager.remove(programaEliminar);
+    }
+        
+}
---
+++ src/main/java/edu/konrad/sistemacalificaciones/persistence/RolPersistence.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.konrad.sistemacalificaciones.persistence;
+
+import edu.konrad.sistemacalificaciones.entity.RolEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * Clase manejadora de la transaccionalidad de la entidad Rol
+ * @author Evelyn Gúzman y Camilo Fique
+ */ 
+@Stateless
+public class RolPersistence {
+     /**
+     * Instancia del entity manager
+     */
+    @PersistenceContext(unitName = "calificacionesPU")
+    private EntityManager entityManager;
+
+    /**
+     * Metodo que trae todos los datos que se encuentran en la tabla Rol
+     * @return lista rol
+     */
+
+    public List<RolEntity> findAll(){
+        Query queryProducto = entityManager.createQuery("select p from Rol p");
+        return queryProducto.getResultList();
+    }
+    
+    /**
+     * Metodo que busca un objeto rol  mediante su id
+     * @param id
+     * @return rol encontrado
+     */
+    public RolEntity find(Long id){
+        return entityManager.find(RolEntity.class, id);
+    }
+
+       
+    /**
+     * Metodo para crear un objeto de la entidad rol
+     * @param rolNuevo
+     * @return rolNuevo
+     */
+    public RolEntity create(RolEntity rolNuevo){
+        entityManager.persist(rolNuevo);
+        return rolNuevo;
+    }
+    
+    /**
+     * Metodo para actualizar un dato de la entidad rol
+     * @param rolActualizar
+     * @return rol actualizado
+     */
+    public RolEntity update(RolEntity rolActualizar){
+        return entityManager.merge(rolActualizar);
+    }
+    
+    /**
+     * Metodo usado para eliminar un rol
+     * @param id 
+     */
+    public void remove(Long id){
+        RolEntity rolEliminar = entityManager.find(RolEntity.class, id);
+        entityManager.remove(rolEliminar);
+    }
+    
+}
