062b9593b148da70b5401d31b1cda696493d3d4f
Evly <evelyny.guzmanc@konradlorenz.edu.co>
2018-10-06 21:20:32 -0500

    Grupo
    DTO, Logic y Resource del grupo

--- src/main/java/edu/konrad/sistemacalificaciones/resources/ApplicationConfig.java
+++ src/main/java/edu/konrad/sistemacalificaciones/resources/ApplicationConfig.java
@@ -27,6 +27,7 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources){
         resources.add(edu.konrad.sistemacalificaciones.resources.CorteResource.class);
         resources.add(edu.konrad.sistemacalificaciones.resources.CuentaResource.class);
+        resources.add(edu.konrad.sistemacalificaciones.resources.GrupoResource.class);
         resources.add(edu.konrad.sistemacalificaciones.resources.ProgramaResource.class);
         resources.add(edu.konrad.sistemacalificaciones.resources.RolResource.class);
 
