f5d9ba12db1405b8ca8b0bbe9f39fb8b2fe73c4b
cafc1234 <camilofique@hotmail.com>
2018-11-22 18:07:48 -0500

    Estudiante
    Se completa funcionalidad estudiante completamente funcional

--- src/main/webapp/js/estudiante/listar/estudiante.listar.ctrl.js
+++ src/main/webapp/js/estudiante/listar/estudiante.listar.ctrl.js
@@ -3,21 +3,23 @@ var estudianteModule = angular.module("estudianteModule");
 
 estudianteModule.controller('estudianteCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
 
-
-        $scope.programasEstudiantes = new Array();
-        $scope.tiposDocumentoEstudiantes = new Array();
-        $scope.estudiantes = new Array();
-        $scope.estudiante = {};
-        $scope.cuenta = {};
-        $scope.tipoDocumento = {};
-        $scope.programa = {};
-        $scope.cuenta.idRol = {
-            "id": 54,
-            "nombreRol": "Estudiante"
-        };
-
         // metodo necesario para que se recargue correctamente la pagina, el $state no esta sirviendo por que recarga y no espera que se termine la ejecucion de la accion
         reload = function () {
+
+            $scope.programasEstudiantes = new Array();
+            $scope.tiposDocumentoEstudiantes = new Array();
+            $scope.estudiantes = new Array();
+            $scope.estudiante = {};
+            $scope.cuenta = {};
+            $scope.tipoDocumento = {};
+            $scope.programa = {};
+            $scope.cuenta.idRol = {
+                "id": 54,
+                "nombreRol": "Estudiante"
+            };
+
+
+
             $http.get('api/programas').then(function (response) {
                 $scope.programasEstudiantes = response.data;
             }, function (error) {
@@ -38,13 +40,51 @@ estudianteModule.controller('estudianteCtrl', ['$scope', '$http', '$state', func
 
         reload();
 
+
+        /*Se encarga de mostrar el modal de confirmación y de asignar el id a eliminar a la variable
+        idEliminar
+         **/
+        $scope.eliminar = function (id) {
+            $scope.idEliminar = id;
+            $("#modalConfirmar").modal("show");
+
+        };
+
+/*
+ *  Se encarga de eliminar el estudiante una vez se confirmó por parte del usuario que desea realizarse
+ */
+        $scope.eliminarEstudiante = function () {
+            $http.delete('api/personas/' + $scope.idEliminar).then(function (response) {
+                $scope.estudiante = {};
+                // se llama el metodo de cierre del modal
+                $scope.cerrarModal();
+
+                reload();
+            }, function (error) {
+                console.log(error);
+            });
+        };
+
         $scope.actualizarCrear = function (id, tipoModal) {
             $scope.tipoModal = tipoModal;
             $scope.idEstudiante = id;
+
+            if (id != 0) {
+                $http.get('api/personas/' + id).then(function (response) {
+                    $scope.estudiante = response.data;
+                    $scope.cuenta = $scope.estudiante.codigo;
+                    $scope.tipoDocumento = $scope.estudiante.tipoDocumento;
+                    $scope.programa = $scope.estudiante.programa;
+                }, function (error) {
+                    console.log(error);
+                });
+            }
+            ;
         };
 
         $scope.cerrarModal = function () {
             $("#modalActualizar").modal("hide");
+            $("#modalConfirmar").modal("hide");
 
         };
 
@@ -54,12 +94,29 @@ estudianteModule.controller('estudianteCtrl', ['$scope', '$http', '$state', func
 
 
         };
+
+        $scope.actualizarEstudiante = function () {
+            if ($scope.estudiante.id && $scope.estudiante.nombre && $scope.estudiante.apellido && $scope.estudiante.correo
+                    && $scope.estudiante.genero && $scope.estudiante.documento && $scope.estudiante.codigo && $scope.estudiante.programa
+                    && $scope.estudiante.tipoDocumento) {
+                $http.put('api/personas/' + $scope.estudiante.id, JSON.stringify($scope.estudiante)).then(function (response) {
+                    $scope.estudiante = {};
+                    // se llama el metodo de cierre del modal
+                    $scope.cerrarModal();
+                    //se recarga la pagina, con el metodo creado que vuelve a llamar el get de los datos
+                    reload();
+                }, function (error) {
+                    console.log(error);
+                });
+            }
+        };
+
+
         $scope.crearEstudianteCompleto = function () {
-            alert("entró crear estudiante");
             if ($scope.estudiante.nombre && $scope.estudiante.apellido && $scope.estudiante.correo
                     && $scope.estudiante.genero && $scope.estudiante.documento && $scope.estudiante.codigo && $scope.estudiante.programa
                     && $scope.estudiante.tipoDocumento) {
-                console.log($scope.estudiante);
+
                 $http.post('api/personas/', JSON.stringify($scope.estudiante)).then(function (response) {
                     $scope.estudiante = {};
                     // se llama el metodo de cierre del modal
